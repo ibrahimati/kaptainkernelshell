c28c669d6ea6924acb815afda3a1cbcda2d27d1c
Chris LoGuidice <chris7775@hotmail.com>
2015-08-31 21:18:57 -0400

    Main skeleton

--- main.c
+++ main.c
@@ -3,35 +3,66 @@
 #include <stdio.h>
 
 
+
 int main()
 {
 	char buffer[256];
-	char delimiters[7] = {'\n',' ', '<', '>', '|', '&'};
+	char delimiters[7] = {'\n', ' ', '<', '>', '|', '&'};
 	int i;
 	int stop = 0;
+	char n_word[5] = "open";
+	int ch_count = 0;
+	char ch;
 
 	while (1)
 	{
+		// ch_count reset
+		ch_count = 0;
 
+		// Shell prompt
 		printf("$HOME:$PWD <> ");
 
-		fgets(buffer, 255, stdin);
+		// Read in command line
+		ch = getchar();
+		while(ch != '\n')
+		{
+			buffer[ch_count] = ch;
+			ch = getchar();
+			ch_count++;
+		}
+		buffer[ch_count] = '\0';
 
-		printf("%s \n", buffer);
+		if(ch_count == 0)
+			continue;
+
+		if(ch_count > 255)
+		{
+			stop = 1;
+		}
+		// Full command line read in
+
+		printf("%s\n", buffer);
 		
+		// First char invalid
 		for(i = 0; i < 7; i++)
 		{
-			if(buffer[i] == delimiters[i])
+			if(buffer[0] == delimiters[i])
 			{
 				stop = 1;
 				break;
 			}	
 		}
 
+		// Exiting shell
 		if(stop == 1)
 		{
+			printf("Exiting shell... \n\n");
 			break;
 		}
+
+		if(n_word == "exit")
+			break;
+		// Exiting shell
 	}
 
 	return 0;
