5013ed019c2ae3d95f007aeabb7ab01b188bcdaa
Chris LoGuidice <chris7775@hotmail.com>
2015-09-02 18:24:22 -0400

    Compiling skeleton of structs and reading in of command line

--- main.c
+++ main.c
@@ -1,47 +1,96 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
 
+//structs
+struct Redirection
+{
+	char **tokens;
+	char **input;
+	char **output;
+};
+
+struct Node
+{
+	char *prev_dir;
+	char *curr_dir;
+	struct Node *children;
+};
+
+// tree 
+struct Directories
+{
+	struct Node *root;
+	int size;
+};
+
+struct Env
+{
+	char *home;
+	char *pwd;
+	char *shell; //??
+	char *user;
+	char *path; //??
+	char *oldpwd;
+};
+//structs
+
+//functions
+char *Read_Line()
+{
+	char *buffer = malloc(sizeof(char)*(256));
+	int stop = 0;
+	int ch_count = 0;
+	char ch;
+
+	// Read in command line
+	ch = getchar();
+	while(ch != '\n')
+	{
+		buffer[ch_count] = ch;
+		ch = getchar();
+		ch_count++;
+	}
+	buffer[ch_count] = '\0';
+
+	if(ch_count > 255)
+	{
+		stop = 1; // run an exit strategy >>>>
+	}
+	// Full command line read in
+
+	return buffer;
+}
+
+
+// Embedded strings
+char *env_var[] = {"$HOME", "$PWD", "$SHELL", "$USER", "$PATH", "$OLDPWD"};
+char *built_in[] = {"exit", "ls", "cd", "echo"};
 
 
 int main()
 {
-	char buffer[256];
+	char *buffer; 
 	char delimiters[7] = {'\n', ' ', '<', '>', '|', '&'};
 	int i;
 	int stop = 0;
-	char n_word[5] = "open";
-	int ch_count = 0;
-	char ch;
 
 	while (1)
 	{
-		// ch_count reset
-		ch_count = 0;
-
 		// Shell prompt
 		printf("$HOME:$PWD <> ");
 
-		// Read in command line
-		ch = getchar();
-		while(ch != '\n')
-		{
-			buffer[ch_count] = ch;
-			ch = getchar();
-			ch_count++;
-		}
-		buffer[ch_count] = '\0';
-
-		if(ch_count == 0)
-			continue;
-
-		if(ch_count > 255)
-		{
-			stop = 1;
-		}
-		// Full command line read in
+		buffer = malloc(sizeof(char)*(256));
+		buffer = Read_Line();
 
 		printf("%s\n", buffer);
+
+		//empty input
+		if(buffer[0] == '\0')
+			continue;
 		
 		// First char invalid
 		for(i = 0; i < 7; i++)
@@ -53,15 +102,18 @@ int main()
 			}	
 		}
 
-		// Exiting shell
+		// Exiting shell but needs to upon finding "exit"
 		if(stop == 1)
 		{
 			printf("Exiting shell... \n\n");
 			break;
 		}
 
+		free (buffer);
+		/*
 		if(n_word == "exit")
-			break;
+			break;*/
+
 		// Exiting shell
 	}
 
