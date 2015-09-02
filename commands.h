5bb3518d2e837c6402d9b8848ee4d3286f6639cf
ibrahimati <ia10@my.fsu.edu>
2015-09-02 12:56:46 -0400

    Create commands.h

---
+++ commands.h
@@ -0,0 +1,22 @@
+/*Class for commands
+Parent class compares and identifies command, exits and prints error message if command not valid
+Then jumps to individual child class based on which command it is.
+7 children:
+echo
+
+exit
+
+cd
+
+ls
+
+more
+
+etime
+
+limits
+
+
+*/
+
+
