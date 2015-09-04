787f1bfe4c5198b5a3129e851b2db5cea29ad6be
ibrahimati <ia10@my.fsu.edu>
2015-09-04 11:47:32 -0400

    Update commands.h
    added exit command and backbone of classes

--- commands.h
+++ commands.h
@@ -19,4 +19,65 @@ limits
 
 */
 
+#include <iostream>
+#include <cstdlib>
 
+
+#ifndef _COMMAND_H
+#define _COMMAND_H
+
+
+class command 
+{
+//constructor for all commands
+
+//identify function that then jumps to appropriate child class
+
+
+};
+
+class exit: public command
+{
+  // Exiting shell
+exit() //constructor
+{
+printf("Exiting shell... \n\n");
+exit(0);
+}
+// Exiting shell
+};
+
+class echo : public command
+{
+  
+};
+
+class cd : public command
+{
+  
+};
+
+class ls : public command
+{
+  
+};
+
+class more : public command
+{
+  
+};
+
+class etime : public command
+{
+  
+};
+
+class limits : public command
+{
+  
+};
+
+
+
+
+#endif
